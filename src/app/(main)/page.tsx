2691b35f44144355415c2824138764768e8d6ff2
Novo1999 <neptune.navo@gmail.com>
2025-09-18 10:19:30 +0600

    remove redirect from page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,8 @@
 import { auth } from '@/auth'
 import Dashboard from '@/components/Dashboard'
-import { redirect } from 'next/navigation'
 
 export default async function Home() {
   const session = await auth()
 
-  if (!session?.user?.email) redirect('/login')
   return <Dashboard user={session?.user} />
 }
